b2cc66490988290004d9df10b229aa28bd57744a
thanhlam <lam.nguyen@pascal.studio>
2022-01-16 18:32:03 +0700

    :bug: redirect wrong when click in other user

--- frontend/src/components/Pin.jsx
+++ frontend/src/components/Pin.jsx
@@ -106,7 +106,7 @@ const Pin = ({ pin }) => {
           </div>
         )}
       </div>
-      <Link to={`user-profile/${user?._id}`} className="flex gap-2 mt-2 items-center">
+      <Link to={`/user-profile/${postedBy?._id}`} className="flex gap-2 mt-2 items-center">
         <img className="w-8 h-8 rounded-full object-cover" src={postedBy?.image} alt="user-profile" />
         <p className="font-semibold capitalize">{postedBy?.userName}</p>
       </Link>
