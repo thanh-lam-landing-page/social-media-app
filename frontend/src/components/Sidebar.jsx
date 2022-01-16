c2e60da9a56cbda2ab5ad0cc3deadc627029d89c
thanhlam <lam.nguyen@pascal.studio>
2022-01-16 17:17:08 +0700

    :sparkles: show created and saved in profile, add image to category in sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import { RiHomeFill } from "react-icons/ri";
-import { IoIosArrowForward, IoMdHeartEmpty } from "react-icons/io";
+
+import { categories } from "../utils/data";
 
 // @ts-ignore
 import logo from "../assets/logo.png";
@@ -9,27 +10,6 @@ import logo from "../assets/logo.png";
 const isNotActiveStyle = "flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize";
 const isActiveStyle =
   "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize";
-
-const categories = [
-  {
-    name: "Animals",
-  },
-  {
-    name: "Wallpapers",
-  },
-  {
-    name: "Photography",
-  },
-  {
-    name: "Gaming",
-  },
-  {
-    name: "Coding",
-  },
-  {
-    name: "Other",
-  },
-];
 
 const Sidebar = ({ user, closeToggle }) => {
   const handleCloseSidebar = () => {
@@ -54,6 +34,7 @@ const Sidebar = ({ user, closeToggle }) => {
               to={`/category/${category.name}`}
               className={({ isActive }) => (isActive ? isActiveStyle : isNotActiveStyle)}
               onClick={handleCloseSidebar}>
+              <img src={category.image} className="w-8 h-8 rounded-full shadow-sm" alt="category" />
               {category.name}
             </NavLink>
           ))}
--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -196,7 +196,7 @@ export const userCreatedPinsQuery = (userId) => {
 }
 
 export const userSavedPinsQuery = (userId) => {
-  const query = `*[_type == "pin" && "${userId} in save[].userId"] | order(_createdAt desc){
+  const query = `*[_type == "pin" && "${userId}" in save[].userId] | order(_createdAt desc){
     image {
       asset->{
         url
